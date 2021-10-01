31692afc8ce2eed073e7c9a31244cad0c2f9012e
aman6177 <cubaasd456@gmail.com>
2021-10-01 10:29:28 -0400

    view card for wallet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ViewCardComponent } from './components/view-card/view-card.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'viewCard', component: ViewCardComponent },
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  showNumberCard = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleCard(): void {
+    this.showNumberCard = !this.showNumberCard;
   }
 
 }
